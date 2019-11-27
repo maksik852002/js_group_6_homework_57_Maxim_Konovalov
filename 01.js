4e346c649e743d315e99524cb66db5cb9cec509e
Maxim Konovalov <maksik852002@gmail.com>
2019-11-27 14:33:50 +0600

    Create structure

---
+++ 01.js
@@ -0,0 +1,11 @@
+const tasks = [
+  {id: 234, title: 'Create user registration API', timeSpent: 4, category: 'Backend', type: 'task'},
+  {id: 235, title: 'Create user registration UI', timeSpent: 8, category: 'Frontend', type: 'task'},
+  {id: 237, title: 'User sign-in via Google UI', timeSpent: 3.5, category: 'Frontend', type: 'task'},
+  {id: 238, title: 'User sign-in via Google API', timeSpent: 5, category: 'Backend', type: 'task'},
+  {id: 241, title: 'Fix account linking', timeSpent: 5, category: 'Backend', type: 'bug'},
+  {id: 250, title: 'Fix wrong time created on new record', timeSpent: 1, category: 'Backend', type: 'bug'},
+  {id: 262, title: 'Fix sign-in failed messages', timeSpent: 2, category: 'Frontend', type: 'bug'},
+ ];
+
+ 
