28dae08798be00821645b02717f3ed50eeed65e9
Maxim Konovalov <maksik852002@gmail.com>
2019-11-28 11:09:31 +0600

    task 1 done

--- 01.js
+++ 01.js
@@ -8,4 +8,10 @@ const tasks = [
   {id: 262, title: 'Fix sign-in failed messages', timeSpent: 2, category: 'Frontend', type: 'bug'},
  ];
 
- 
+ console.log(`Общее количество времени, затраченное на работу над задачами из категории 'Frontend': ${tasks.reduce((acc, el) => el.category==='Frontend' ? acc + el.timeSpent :acc,0)} часов.`);
+ console.log(`Общее количество времени, затраченное на работу над задачами типа 'bug': ${tasks.reduce((acc, el) => el.type==='bug' ? acc + el.timeSpent : acc,0)} часов.`);
+ console.log(`Количество задач, имеющих в названии слово "UI": ${tasks.reduce((acc,el) => el.title.includes('UI') ? acc+1: acc, 0)} задачи.`);
+ console.log('Получите количество задач каждой категории в объект вида:');
+ console.log(tasks.reduce((acc, el) => {acc[el.category]=(acc[el.category] || 0)+1; return acc}, {}));
+ console.log('Массив задач с затраченным временем больше 4 часов. В массиве должны быть объекты только с ключами title и category:');
+ console.log(tasks.reduce((acc,el) => el.timeSpent > 4 ? acc.concat([{title:el.title, category:el.category}]) : acc,[]));
